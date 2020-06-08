63c64ade4fdaf8c511df1ee3f5245f87655581a7
NeverTh <neverth0@foxmail.com>
2020-06-08 13:00:41 +0800

    42. 接雨水

--- leetcode/src/main/java/fun/neverth/Part2.java
+++ leetcode/src/main/java/fun/neverth/Part2.java
@@ -647,9 +647,71 @@ public class Part2 {
         return left;
     }
 
+    /**
+     * 42. 接雨水
+     */
+    public int trap(int[] height) {
+
+        int len = height.length;
+        int res = 0;
+
+        int[] left_max = new int[len];
+        int[] right_max = new int[len];
+
+        left_max[0] = height[0];
+        right_max[len - 1] = height[len - 1];
+
+        for(int i = 1; i < len; i++){
+            left_max[i] = Math.max(left_max[i - 1], height[i]);
+        }
+
+        for(int i = len - 2; i >= 0; i--){
+            right_max[i] = Math.max(right_max[i + 1], height[i]);
+        }
+
+        for (int i = 1; i < len - 1; i++){
+            res += Math.min(left_max[i], right_max[i]) - height[i];
+        }
+
+        return res;
+    }
+
+    public int trap1(int[] height) {
+
+        int len = height.length;
+        if(len == 0){
+            return 0;
+        }
+        int res = 0;
+
+        int left = 0, right = len - 1;
+
+        int left_max = height[0];
+        int right_max = height[len - 1];
+
+        while(left <= right){
+
+            left_max = Math.max(left_max, height[left]);
+            right_max = Math.max(right_max, height[right]);
+
+            if (left_max < right_max){
+                res += left_max - height[left];
+                left++;
+
+
+            } else{
+                res += right_max - height[left];
+                right--;
+
+            }
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         Part2 part2 = new Part2();
-        int[] a = {1,2,3,4,5,6,7,8,9,10};
+        int[] a = {0,1,0,2,1,0,1,3,2,1,2,1};
         String[] b = {"0000"};
 
         int[][] c = new int[][]{{1, 2, 3}, {5, 4, 0}};
@@ -666,7 +728,7 @@ public class Part2 {
         n3.left = n4;
         n3.right = n5;
 
-        System.out.println(part2.shipWithinDays(a, 5));
+        System.out.println(part2.trap1(a));
 
     }
 }
