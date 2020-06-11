a6a9a91cd1fc84aacb9c036f01931504d5a4e6be
NeverTh <neverth0@foxmail.com>
2020-06-11 19:50:22 +0800

    面试题03. 数组中重复的数字

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -38,4 +38,65 @@ public class Part3 {
         return res;
     }
 
+    /**
+     * 面试题03. 数组中重复的数字
+     */
+    public int findRepeatNumber(int[] nums) {
+        int len = nums.length;
+
+        for(int i = 0; i < len; i++){
+
+            while(nums[i] != i){
+                if(nums[i] == nums[nums[i]]){
+                    return nums[i];
+                }
+                swap(nums, i, nums[i]);
+            }
+        }
+        return -1;
+    }
+
+    public void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+
+        nums[a] = nums[b];
+
+        nums[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        Part3 part3 = new Part3();
+
+        int[] a = {1, 1, 1, 1, 2, 2, 3};
+        String[] b = {"0000"};
+
+        int[][] c = new int[][]{{1, 2, 3}, {5, 4, 0}};
+
+
+        Common.TreeNode n1 = new Common.TreeNode(3);
+        Common.TreeNode n2 = new Common.TreeNode(9);
+        Common.TreeNode n3 = new Common.TreeNode(20);
+        Common.TreeNode n4 = new Common.TreeNode(15);
+        Common.TreeNode n5 = new Common.TreeNode(7);
+
+        Part2.ListNode l1 = new Part2.ListNode(1);
+        Part2.ListNode l2 = new Part2.ListNode(2);
+        Part2.ListNode l3 = new Part2.ListNode(3);
+        Part2.ListNode l4 = new Part2.ListNode(4);
+        Part2.ListNode l5 = new Part2.ListNode(3);
+
+        l1.next = null;
+        l2.next = null;
+        l3.next = null;
+        l4.next = null;
+
+        n1.left = n2;
+        n1.right = n3;
+        n3.left = n4;
+        n3.right = n5;
+
+        System.out.println(part3.maxSubArray(a));
+
+    }
+
 }
