7acd6f359f59bf27b0efef7bb3fe135dbad4ae88
NeverTh <neverth0@foxmail.com>
2020-06-11 18:10:04 +0800

    53. 最大子序和

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -17,5 +17,25 @@ public class Part3 {
         }
     }
 
+    /**
+     * 53. 最大子序和
+     */
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+
+        int[] dp = new int[len];
+
+        dp[0] = nums[0];
+
+        for (int i = 1; i < len; i++){
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        }
+
+        int res = Integer.MIN_VALUE;
+        for (int value: dp){
+            res = Math.max(value, res);
+        }
+        return res;
+    }
 
 }
