ec9235bb5f3546813a90b724f2153857a4506f12
NeverTh <neverth0@foxmail.com>
2020-06-12 12:25:41 +0800

    面试题05. 替换空格

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -185,6 +185,24 @@ public class Part3 {
         return false;
     }
 
+
+    /**
+     * 面试题05. 替换空格
+     */
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == ' '){
+                sb.append("%20");
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         Part3 part3 = new Part3();
 
