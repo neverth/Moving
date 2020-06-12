725a6456eeb8af89bb46eef45f432e4bf50a279f
NeverTh <neverth0@foxmail.com>
2020-06-12 10:37:30 +0800

    287. 寻找重复数 剑指offer-数组中重复的数字-不修改原数组

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -39,7 +39,7 @@ public class Part3 {
     }
 
     /**
-     * 面试题03. 数组中重复的数字
+     * 面试题03. 剑指offer-数组中重复的数字-修改原数组
      */
     public int findRepeatNumber(int[] nums) {
         int len = nums.length;
@@ -64,10 +64,40 @@ public class Part3 {
         nums[b] = temp;
     }
 
+    /**
+     * 287. 寻找重复数 剑指offer-数组中重复的数字-不修改原数组
+     */
+    public int findDuplicate(int[] nums) {
+        int len = nums.length;
+
+        int left = 1, right = len - 1;
+
+        while(left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            int n = 0;
+            for(int value: nums){
+                if(value <= mid){
+                    n++;
+                }
+            }
+
+            if(n > mid){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+
+        }
+
+        return left;
+    }
+
     public static void main(String[] args) {
         Part3 part3 = new Part3();
 
-        int[] a = {1, 1, 1, 1, 2, 2, 3};
+        int[] a = {1,3,4,2,2};
         String[] b = {"0000"};
 
         int[][] c = new int[][]{{1, 2, 3}, {5, 4, 0}};
@@ -95,7 +125,7 @@ public class Part3 {
         n3.left = n4;
         n3.right = n5;
 
-        System.out.println(part3.maxSubArray(a));
+        System.out.println(part3.findDuplicate(a));
 
     }
 
