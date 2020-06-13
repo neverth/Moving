364c06e7defeff777752658087807635c1efaf85
NeverTh <neverth0@foxmail.com>
2020-06-13 15:37:01 +0800

    面试题12. 矩阵中的路径 && 79. 单词搜索

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -1,5 +1,7 @@
 package fun.neverth;
 
+import com.sun.deploy.net.proxy.ProxyUnavailableException;
+
 import java.util.*;
 
 /**
@@ -524,6 +526,58 @@ public class Part3 {
         }
         return -1;
     }
+
+    /**
+     * 面试题12. 矩阵中的路径 && 79. 单词搜索
+     */
+    public boolean exist(char[][] board, String word) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+
+                    if (backtrack(board, word, i, j, 0)) {
+                        return true;
+                    }
+
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean backtrack(char[][] board, String word, int i, int j, int k) {
+
+        if (
+                i >= board.length
+                || i < 0
+                || j >= board[0].length
+                || j < 0
+                || board[i][j] != word.charAt(k)
+        ) {
+
+            return false;
+        }
+
+        if (k == word.length() - 1) {
+            return true;
+        }
+
+        char temp = board[i][j];
+
+        board[i][j] = '0';
+
+        boolean res = backtrack(board, word, i + 1, j, k + 1)
+                || backtrack(board, word, i - 1, j, k + 1)
+                || backtrack(board, word, i, j + 1, k + 1)
+                || backtrack(board, word, i, j - 1, k + 1);
+
+        board[i][j] = temp;
+
+        return res;
+    }
+
 
     public static void main(String[] args) {
         Part3 part3 = new Part3();
