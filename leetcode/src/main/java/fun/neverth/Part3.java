0270139f3fbe06376c9e6e509a44686d009ba70e
NeverTh <neverth0@foxmail.com>
2020-06-13 12:52:28 +0800

    面试题10- II. 青蛙跳台阶问题 && 70. 爬楼梯

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -444,6 +444,27 @@ public class Part3 {
 
         return dp[n];
     }
+
+    /**
+     * 面试题10- II. 青蛙跳台阶问题 && 70. 爬楼梯
+     */
+    public int numWays(int n) {
+        if(n == 0 || n == 1){
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] = dp[i] % 1000000007;
+        }
+
+        return dp[n];
+    }
     public static void main(String[] args) {
         Part3 part3 = new Part3();
 
