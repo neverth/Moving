2f4959c32cb8294cc8b7184bc0b427826fa71fef
NeverTh <neverth0@foxmail.com>
2020-06-13 12:40:30 +0800

    面试题10- I. 斐波那契数列 && 509. 斐波那契数

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -423,6 +423,27 @@ public class Part3 {
         return res;
     }
 
+    /**
+     * 面试题10- I. 斐波那契数列 && 509. 斐波那契数
+     */
+    public int fib(int n) {
+
+        if(n == 0){
+            return 0;
+        }
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] = dp[i] % 1000000007;
+        }
+
+        return dp[n];
+    }
     public static void main(String[] args) {
         Part3 part3 = new Part3();
 
