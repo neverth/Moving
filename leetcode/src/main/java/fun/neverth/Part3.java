b081d501d1b4e9e397cc68cb7e47b8be9970ea2d
NeverTh <neverth0@foxmail.com>
2020-06-19 13:06:34 +0800

    复习 1143. 最长公共子序列

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -926,6 +926,42 @@ public class Part3 {
         return ans;
     }
 
+    public void printDp(int[][] dp) {
+
+        for (int[] ints : dp) {
+            for (int anInt : ints) {
+
+                System.out.print(anInt);
+                System.out.print('\t');
+            }
+            System.out.println();
+        }
+        System.out.println("------------------");
+    }
+
+    /**
+     * 复习 1143. 最长公共子序列
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        char[] ch1 = text1.toCharArray();
+        char[] ch2 = text2.toCharArray();
+
+        int[][] dp = new int[ch1.length + 1][ch2.length + 1];
+
+        for(int i = 1; i <= ch1.length; i++){
+            for(int j = 1; j <= ch2.length; j++){
+
+                if(ch1[i - 1] == ch2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+
+            }
+        }
+//        printDp(dp);
+        return dp[ch1.length][ch2.length];
+    }
     public static void main(String[] args) {
         Part3 part3 = new Part3();
 
@@ -963,7 +999,7 @@ public class Part3 {
         n1.right = n3;
         n3.left = n4;
         n3.right = n5;
-        System.out.println(part3.isMatch("ab", ".*"));
+        System.out.println(part3.longestCommonSubsequence("bl", "yby"));
 
     }
 
