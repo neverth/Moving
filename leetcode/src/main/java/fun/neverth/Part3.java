b4e01c441cc4895aeec9a3644041df29b2dbbc74
NeverTh <neverth0@foxmail.com>
2020-06-19 13:36:56 +0800

    复习 300. 最长上升子序列

--- leetcode/src/main/java/fun/neverth/Part3.java
+++ leetcode/src/main/java/fun/neverth/Part3.java
@@ -962,6 +962,32 @@ public class Part3 {
 //        printDp(dp);
         return dp[ch1.length][ch2.length];
     }
+
+    /**
+     * 复习 300. 最长上升子序列
+     */
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Part3 part3 = new Part3();
 
