719b28eb6a856dfe59dc6fd2f6be9e8f37965113
NeverTh <neverth0@foxmail.com>
2020-07-06 16:19:28 +0800

    复习 25. K 个一组翻转链表

--- leetcode/src/main/java/fun/neverth/Part4.java
+++ leetcode/src/main/java/fun/neverth/Part4.java
@@ -284,6 +284,39 @@ public class Part4 {
         return cur;
     }
 
+    public ListNode reverse(ListNode a, ListNode b){
+        ListNode pre, cur, next;
+        pre = null; cur = a; next = a;
+        while (cur != b) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+
+    }
+
+    /**
+     * 复习 25. K 个一组翻转链表
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null){
+            return null;
+        }
+        ListNode a, b;
+        a = b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null){
+                return head;
+            }
+            b = b.next;
+        }
+        ListNode newHead = reverse(a, b);
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
+
     public static void main(String[] args) {
         Part4 part4 = new Part4();
 
